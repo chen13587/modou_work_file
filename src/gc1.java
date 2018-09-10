dfaa5e6cb30019728b1dbdfdf920b1a90cbb1117
chen13587 <1356764219@qq.com>
2018-09-10 21:19:00 +0800

    chen

---
+++ src/gc1.java
@@ -0,0 +1,47 @@
+
+public class gc1{
+    static int age=1;
+    static boolean z=true;
+    static String str="hello world";
+    public static void main(String[] args){
+        if(z==true) {
+            System.out.println(str);
+        }
+        string a=new string();
+        a.output(str);
+        cla_1 a1=new cla_1();
+        System.out.println(a1.a(7));
+
+    }
+
+}
+
+class string{
+
+    public static void output(String s){
+       System.out.println(s);
+
+    }
+}
+
+class cla_1{
+    public static String a(int i){
+        return "hello yxy: "+i;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
